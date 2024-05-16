8b5b46ca0eced5f036e0a8b6daba02d750875622
Andy Ge <andyshge@gmail.com>
2024-05-16 18:12:17 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import NavBar from './Pages/Home/NavBar';
 import Home from "./Pages/Home/Homescreen"
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <div>
+          <NavBar/>
+          <Routes>
+            <Route path ="/" element={<Home />}></Route>
+            <Route path ="*" element={<div>404 Not Found</div>}></Route>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
