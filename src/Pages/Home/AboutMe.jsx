f7b31a2b0a2017f256d89f8531bcaf558d3ba31e
Andy Ge <andyshge@gmail.com>
2024-09-27 15:03:39 -0400

    latest update

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -10,11 +10,21 @@ export default function AboutMe() {
                 <div className="hero--section--content">
                     <h1 className="skills-section--heading">About Me</h1>
                     <p className="hero--section--description">
-                    Im a curriculum lead at FirstByte, a club at Northeastern University that promotes diversity and equity in STEM by teaching and inspiring interest 
-                    in computer science within the Boston community. I also work in a robotics research lab at Northeastern, where Im developing a growing interest in 
-                    gaining insights from robotics data. My current project involves creating autonomous home assistant robots that predict user needs based on behavior 
-                    and environmental data. As a lifelong learner from an immigrant household, I value collaborating with like-minded individuals and continuously strive 
-                    to deepen my understanding of both technical and personal subjects.
+                    What would you do if you could predict the future? No matter what you're interested in, the possibilites are endless. Now you might be a little confused 
+                    about why I'm asking this question when you're supposed to be learning about me. As I progressed through my college career and developed deeper skills in 
+                    data science and statistical modelling, I became captivated with the power of using data to model outcomes across almost any field or topic. This pursuit 
+                    of the ability to predict future events through statistcal modelling and machine learning is what drives me towards the field of machine learning engineering. 
+                    <br /><br />
+                    I'm currently working in a robotics research lab at Northeastern University that aims to further work being done in human robot interaction. In my current 
+                    project I am working on creating machine learning models that predict the home routines of users based on behavior and environmental data for autonomous home 
+                    robot assistants. Through improving these models, I hope to further the work being done in longitudinal proactive robot assistance, aiming for a future where
+                    the elderly or disabled can live more independently with robot home assistants which can independently serve their needs.
+                    <br /><br />
+                    In my free time I enjoy watching and playing sports, cooking, and teaching computer science to students in the local Boston community through FirstByte,
+                    club at Northeastern University, where I served as President for 2 years, that promotes diversity and equity in STEM by providing equitable access to 
+                    computer science education. When I have a longer break, I love to travel, on the left here is a picture of me when I visited Malaga during my study abroad
+                    (if you're ever there you have to try the restaurant Casa Lola). A couple of the big items on my bucket list are to visit all the National Parks and the 
+                    Seven Wonders of the Ancient World!
                     </p>
                 </div>
             </div>
--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -14,7 +14,9 @@ export default function MySkills() {
                         </div>
                         <div className="skills--section--card--content">
                             <h3 className="skills--section--title">{item.title}</h3>
-                            <p className="skills--section--description">{item.description}</p>
+                            <p className="skills--section--description">
+                                {item.description}
+                            </p>
                         </div>
                     </div>
                 ))}
